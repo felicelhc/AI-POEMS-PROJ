50233b6e13ecc3139f07d5766832ff827c7d8d1d
Felice <fishfillet2fab4u@gmail.com>
2024-01-01 16:43:17 +0000

    API integration
    - minor HTML changes
    - AI API integration
    - axios imported
    - new function added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem-space", {
-    strings:
-      "No longer do I listen for the scrape of a keel, On the blank stones of the landing.",
+    strings: response.data.answer,
     autoStart: true,
     cursor: " ",
     delay: 40,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructions = document.querySelector("#prompt-input");
+  let apiKey = "98t4637f40718369c12fo73b34e16a06";
+  let prompt = `Please generate a short 6-line poem about ${instructions.value}.`;
+  let context =
+    "You are a romantic poet. Generate poem in HTML format and separate each and all sentences with <br />. You must use rhyming couplets for the last 2 lines. All text align left and do not generate a title. Sign off with '- SheCodes AI'.";
+  let poem = document.querySelector("#poem-space");
+  poem.innerHTML = "Work in progress...";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poem-form");
