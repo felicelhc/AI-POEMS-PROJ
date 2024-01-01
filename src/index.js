7ceaee609cdef6cc9aca587281e98e305c7d8d9f
Felice <fishfillet2fab4u@gmail.com>
2024-01-01 18:52:02 +0000

    prompt altered

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ function generatePoem(event) {
   event.preventDefault();
   let instructions = document.querySelector("#prompt-input");
   let apiKey = "98t4637f40718369c12fo73b34e16a06";
-  let prompt = `Please generate a short 6-line poem about ${instructions.value}.`;
+  let prompt = `Please generate a poem about ${instructions.value} within 6 lines.`;
   let context =
-    "You are a romantic poet. Generate all text in HTML format and separate each and all sentences with <br />. You must use rhyming couplets for the last 2 lines. All text align left and do not generate a title. Sign off with '- SheCodes AI'.";
+    "You are a romantic poet. Be concise and use as little words as possible. Generate all text in HTML format and separate all sentences with <br />.  All text align left and do not generate a title. Sign off with '- SheCodes AI'.";
   let poem = document.querySelector("#poem-space");
   poem.innerHTML = `<div class="blink"> ✨Generating a poem about ${instructions.value}...</div>`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
