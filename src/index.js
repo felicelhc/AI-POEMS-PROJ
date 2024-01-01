a4c4040ca1b5af820ada34bf50ad556d14546c44
Felice <fishfillet2fab4u@gmail.com>
2024-01-01 13:11:48 +0000

    Js added
    - new function created
    - form submit js
    - typewriter effect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-space", {
+    strings:
+      "No longer do I listen for the scrape of a keel, On the blank stones of the landing.",
+    autoStart: true,
+    cursor: " ",
+    delay: 40,
+  });
+}
+
+let poemForm = document.querySelector("#poem-form");
+poemForm.addEventListener("submit", generatePoem);
